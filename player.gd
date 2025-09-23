e6458fe73aee382702a3d4568cedf6b9fab65730
V3AI-Seven <grayson@vantilborg.ca>
2025-09-23 16:02:54 -0400

    Camera motion now works

--- player.gd
+++ player.gd
@@ -8,11 +8,11 @@ const mouse_sensitivity = 0.002
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
-func _input(event):
+func _unhandled_input(event):
 	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-		rotate_y(-event.relative.x * mouse_sensitivity)
-		$Camera3D.rotate_x(-event.relative.y * mouse_sensitivity)
-		$Camera3D.rotation.x = clampf($Camera3D.rotation.x, -deg_to_rad(70), deg_to_rad(70))
+		$Marker3D.rotate_y(-event.relative.x * mouse_sensitivity)
+		$Marker3D/Camera3D.rotate_x(-event.relative.y * mouse_sensitivity)
+		$Marker3D/Camera3D.rotation.x = clampf($Marker3D/Camera3D.rotation.x, -deg_to_rad(70), deg_to_rad(70))
 
 func _physics_process(_delta):
 	var targetVelocity = Vector3.ZERO
