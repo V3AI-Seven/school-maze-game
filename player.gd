f7de21fabafcebd9ca1c59bb2f444ee7c40f5234
Grayson Vantilborg <127050193+V3AI-Seven@users.noreply.github.com>
2025-09-23 15:36:02 -0400

    main project upload

---
+++ player.gd
@@ -0,0 +1,37 @@
+extends CharacterBody3D
+
+const speed = 10
+const sprintSpeed = 15
+const mouse_sensitivity = 0.002
+
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _input(event):
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		$Camera3D.rotate_x(-event.relative.y * mouse_sensitivity)
+		$Camera3D.rotation.x = clampf($Camera3D.rotation.x, -deg_to_rad(70), deg_to_rad(70))
+
+func _physics_process(_delta):
+	var targetVelocity = Vector3.ZERO
+	var worldTargetVelocity = Vector3.ZERO
+	var targetSpeed = speed
+	
+	if Input.is_key_pressed(KEY_SHIFT):
+		targetSpeed = sprintSpeed
+
+	if Input.is_key_pressed(KEY_W):
+		targetVelocity.z -= targetSpeed
+	if Input.is_key_pressed(KEY_S):
+		targetVelocity.z += targetSpeed
+	if Input.is_key_pressed(KEY_A):
+		targetVelocity.x -= targetSpeed
+	if Input.is_key_pressed(KEY_D):
+		targetVelocity.x += targetSpeed
+		
+	worldTargetVelocity = global_transform.basis * targetVelocity
+
+	velocity = worldTargetVelocity
+	move_and_slide()
