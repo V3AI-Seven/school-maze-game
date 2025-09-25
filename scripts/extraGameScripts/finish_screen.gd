1f678c7a73618e4d63b0231d8af9a03c5f6c8eec
V3AI-Seven <127050193+V3AI-Seven@users.noreply.github.com>
2025-09-25 19:43:32 -0400

    Finish maze and add some sound effects.

--- scripts/extraGameScripts/finish_screen.gd
+++ scripts/extraGameScripts/finish_screen.gd
@@ -2,16 +2,21 @@ extends Control
 
 func time_convert(time_in_sec):
 	var seconds = int(time_in_sec)%60
+	@warning_ignore("integer_division")
 	var minutes = (int(time_in_sec)/60)%60
+	@warning_ignore("integer_division")
 	var hours = (int(time_in_sec)/60)/60
 	
 	#returns a string with the format "HH:MM:SS"
 	return "%02d:%02d:%02d" % [hours, minutes, seconds]
 
 func _ready() -> void:
+	
 	if Game.game_won:
+		$SuccessFanfareTrumpets6185.play()
 		$winOrLoseInsult.text = "You Win!"
 		$timer.text = "Congratulations! Life wasted: " + time_convert(Game.timer)
 	else:
+		$CartoonFailTrumpet278822.play()
 		$winOrLoseInsult.text = "You Lose!"
 		$timer.text = "You suck! Life wasted: " + time_convert(Game.timer)
--- scripts/mainGameScripts/maze.gd
+++ scripts/mainGameScripts/maze.gd
@@ -1,1 +1,6 @@
 extends Node3D
+
+func player_left_maze(_body) -> void:
+	Game.game_won = true
+	Game.timer_on = false
+	get_tree().change_scene_to_file("res://scenes/menus/finish_screen.tscn")
