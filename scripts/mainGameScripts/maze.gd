64e63d543977057c97b01da81b57d5daacd8e711
V3AI-Seven <127050193+V3AI-Seven@users.noreply.github.com>
2025-09-25 20:35:01 -0400

    Creepy Outer Wilds background music

--- scripts/mainGameScripts/maze.gd
+++ scripts/mainGameScripts/maze.gd
@@ -1,6 +1,9 @@
 extends Node3D
 
+
+
 func player_left_maze(_body) -> void:
+	$BackgroundMusic.stop()
 	Game.game_won = true
 	Game.timer_on = false
 	get_tree().change_scene_to_file("res://scenes/menus/finish_screen.tscn")
